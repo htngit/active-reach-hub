0f0872ecfa3e4b1c743f1e08e181d103b05399bb
htngit <htn.git@gmail.com>
2025-06-15 12:27:08 +0700

    Erase 0 on field.onchange target value

--- src/components/InvoiceManager/InvoiceDetailsCard.tsx
+++ src/components/InvoiceManager/InvoiceDetailsCard.tsx
@@ -103,7 +103,7 @@ export const InvoiceDetailsCard: React.FC<InvoiceDetailsCardProps> = ({
                   min="0"
                   max="100"
                   {...field}
-                  onChange={(e) => field.onChange(parseFloat(e.target.value) || 0)}
+                  onChange={(e) => field.onChange(parseFloat(e.target.value))}
                 />
               </FormControl>
               <FormMessage />
